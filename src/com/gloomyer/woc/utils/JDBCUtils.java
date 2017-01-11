4f76204565a4dc7a8f5cb33e06122f7bd79697ae
Gloomy <gloomyneter@gmail.com>
2017-01-11 13:55:34 +0800

    import database connection pool

--- src/com/gloomyer/woc/utils/JDBCUtils.java
+++ src/com/gloomyer/woc/utils/JDBCUtils.java
@@ -1,39 +1,26 @@
 package com.gloomyer.woc.utils;
 
 
-import java.io.*;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
  * Created by Gloomy on 2016/10/27.
  */
 public class JDBCUtils {
-    public static final String url = "jdbc:mysql://localhost/woc";
-    public static final String name = "com.mysql.jdbc.Driver";
-    public static String user;
-    public static String password;
+    public static final ComboPooledDataSource dataSource;
 
     static {
-        BufferedReader bis = null;
+        dataSource = new ComboPooledDataSource();
         try {
-            Class.forName(name);
-            String path = Thread.currentThread().getContextClassLoader().getResource("").toString();
-            //path = path.replace('/', '\\'); // 将/换成\
-            path = path.replace("file:", ""); //去掉file:
-            path = path.replace("classes/", ""); //去掉class\
-            path = path.substring(1); //去掉第一个\,如 \D:\JavaWeb...
-            path += "jdbc.txt";
-            path = "/" + path;
-
-            bis = new BufferedReader(new FileReader(new File(path)));
-            String line = bis.readLine();
-            String[] split = line.split("&");
-            user = split[0];
-            password = split[1];
-            System.out.println("user:" + user + ",pwd:" + password);
+//            dataSource.setDriverClass("com.mysql.jdbc.Driver");
+//            dataSource.setJdbcUrl("jdbc:mysql:///woc");
+//            dataSource.setUser("root");
+//            dataSource.setPassword("123456");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -47,8 +34,8 @@ public class JDBCUtils {
      */
     public static Connection getConn() {
         try {
-            return DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
